8914d0e0bb1a400ff5bb4896a0a2571c27f94d76
Konrad Krawczyk <konrad.krawczyk@nyu.edu>
2019-04-01 20:57:33 +0800

    CORS

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from flask_restful import Api
 from flask_cors import CORS
 from controllers import helloController
@@ -23,12 +24,13 @@ db = SQLAlchemy(app)
 api.add_resource(helloController.HelloController, '/api/predict')
 
 @app.after_request
-
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  return response
+    white_origin= ['https://faces-frontend.herokuapp.com','http://localhost']
+    if request.headers['Origin'] in white_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] 
+        response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
